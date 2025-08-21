d0fcb4a0b514cc7b24f443513e8f60c7fcef6fc3
25HP7061 25HP7061 <25HP7061@G302-CC0162.local>
2025-08-21 17:35:41 +0800

    First commit - added my game

--- main.js
+++ main.js
@@ -11,13 +11,12 @@ const getRandomInt = (min, max) => {
   return Math.floor(Math.random() * (max - min) + min);
 };
 
+// Generate food not on the snake
 const generateFood = () => {
   let newFood;
   do {
     newFood = { x: getRandomInt(0, HEIGHT), y: getRandomInt(0, WIDTH) };
-  } while (
-    snake.some((segment) => segment.x === newFood.x && segment.y === newFood.y)
-  );
+  } while (snake.some(segment => segment.x === newFood.x && segment.y === newFood.y));
   return newFood;
 };
 
@@ -47,21 +46,19 @@ const drawBoard = () => {
   const foodEl = document.querySelector(`[x="${food.x}"][y="${food.y}"]`);
   foodEl.classList.add("food");
 
-  for (let i = 0; i < snake.length; i++) {
-    const dot = snake[i];
-    if (i === 0) {
-      const headEl = document.querySelector(`[x="${dot.x}"][y="${dot.y}"]`);
-      headEl.classList.add("head");
-    } else {
-      const bodyEl = document.querySelector(`[x="${dot.x}"][y="${dot.y}"]`);
-      bodyEl.classList.add("body");
-    }
-  }
+  snake.forEach((dot, i) => {
+    const el = document.querySelector(`[x="${dot.x}"][y="${dot.y}"]`);
+    if (i === 0) el.classList.add("head");
+    else el.classList.add("body");
+  });
 };
 
-setInterval(() => {
+drawBoard();
+
+const gameLoop = () => {
   const newSnake = [];
 
+  // Move head depending on direction
   if (DIRECTION === "RIGHT") {
     newSnake[0] = { x: snake[0].x, y: (snake[0].y + 1) % WIDTH };
   } else if (DIRECTION === "LEFT") {
@@ -76,38 +73,30 @@ setInterval(() => {
     newSnake[0] = { x: nextX, y: snake[0].y };
   }
 
+  // Add rest of the body
   for (let i = 0; i < snake.length - 1; i++) {
     newSnake.push(snake[i]);
   }
 
-  if (newSnake[0].x === food.x && newSnake[0].y === food.y) {
-    newSnake.push(snake[snake.length - 1]);
-    food = generateFood();
+  const head = newSnake[0];
+
+  // Check if snake eats food
+  if (head.x === food.x && head.y === food.y) {
+    newSnake.push(snake[snake.length - 1]); // grow snake
+    food = generateFood(); // move food
   }
 
   snake = newSnake;
   drawBoard();
-}, 200);
+};
 
-drawBoard();
+setInterval(gameLoop, 200);
 
-setInterval(() => {
-  const newSnake = [];
-  
-  if (DIRECTION === "RIGHT") {
-    newSnake[0] = { x: snake[0].x, y: (snake[0].y + 1) % WIDTH };
-  } else if (DIRECTION === "LEFT") {
-    let nextY = snake[0].y - 1;
-    if (nextY === -1) nextY = WIDTH - 1;
-    newSnake[0] = { x: snake[0].x, y: nextY };
-  } else if (DIRECTION === "BOTTOM") {
-    newSnake[0] = { x: (snake[0].x + 1) % HEIGHT, y: snake[0].y };
-  } else if (DIRECTION === "TOP") {
-    let nextX = snake[0].x - 1;
-    if (nextX === -1) nextX = HEIGHT - 1;
-    newSnake[0] = { x: nextX, y: snake[0].y };
-  }
-
-  snake = newSnake;
-  drawBoard();
-}, 200);
+// Direction control
+window.addEventListener("keydown", (e) => {
+  const key = e.key.toLowerCase();
+  if ((key === "arrowup" || key === "w") && DIRECTION !== "BOTTOM") DIRECTION = "TOP";
+  else if ((key === "arrowdown" || key === "s") && DIRECTION !== "TOP") DIRECTION = "BOTTOM";
+  else if ((key === "arrowright" || key === "d") && DIRECTION !== "LEFT") DIRECTION = "RIGHT";
+  else if ((key === "arrowleft" || key === "a") && DIRECTION !== "RIGHT") DIRECTION = "LEFT";
+});
